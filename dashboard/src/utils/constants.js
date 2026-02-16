5a715c84cfce45cd7137a5464f519ce2908e1593
pavan-nomula <n.pavandurgasaicharan1611@gmail.com>
2026-02-16 22:26:51 +0530

    feat: update backend API endpoint to onrender.com and introduce constants file for API base URL and refresh interval.

--- dashboard/src/utils/constants.js
+++ dashboard/src/utils/constants.js
@@ -1,4 +1,4 @@
 // export const API_BASE_URL = 'http://localhost:4001/api';
-export const API_BASE_URL = 'https://smart-attendance-backend-beta.vercel.app/api';
+export const API_BASE_URL = 'https://smart-attendance-dashboard-backend.onrender.com/api';
 
 export const REFRESH_INTERVAL = 5000; // 5 seconds
